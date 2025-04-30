e7dba8dfbfc68614975e467657e387e24693c05a
Murilo Felipe <felipemurilo6@gmail.com>
2025-04-30 00:03:41 -0300

    criacao de executavel do back-end com arquivo .jar usando um run.bat, criacao da aba Barbearia na interface, correcao de bugs e melhoria em arquivos javaScript do projeto

---
+++ Back-End (Spring)/demo/run.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Iniciando o aplicativo...
+java -jar target/demo-0.0.1-SNAPSHOT.jar
+pause
---
+++ Back-End (Spring)/demo/src/main/resources/data.sql
@@ -0,0 +1,33 @@
+CREATE TABLE cliente (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(14) NOT NULL
+);
+
+CREATE TABLE servico (
+    id_servico INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(50),
+    valor DECIMAL(5,2)
+);
+
+CREATE TABLE barbeiro (
+    id_barbeiro INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(14) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    data_contratacao DATE NOT NULL,
+    ativo BOOLEAN NOT NULL,
+    comissao DECIMAL(5,2)
+);
+
+CREATE TABLE agendamento (
+    id_agendamento INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT,
+    id_servico INT,
+    id_barbeiro INT,
+    data_hora DATETIME,
+    stats BOOLEAN,
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_barbeiro) REFERENCES barbeiro(id_barbeiro),
+    FOREIGN KEY (id_servico) REFERENCES servico(id_servico)
+);
